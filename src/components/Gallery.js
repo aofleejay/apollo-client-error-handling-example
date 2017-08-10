11d270551f9dc9d8ee9a647e1dacb5e1180ab151
kunapot <kunapot@thinknet.co.th>
2017-08-10 17:35:34 +0700

    base code, wrap component with query HOC

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,53 +1,18 @@
 import React, { Component } from 'react'
-import { graphql, compose } from 'react-apollo'
+import { graphql } from 'react-apollo'
 import { Button } from 'antd'
-import mutationState from 'react-apollo-mutation-state'
 import Loading from './common/Loading'
 import Error from './common/Error'
 import { fetchAllImages as query } from '../queries/image'
-import { createImage as mutation } from '../mutations/image'
 
 class Gallery extends Component {
-  createImageAndReFetch = () => {
-    this.props.mutation.set({ loading: true })
-    this.props.createImage({
-      refetchQueries: [{ query }]
-    })
-    .then(response => {
-      this.props.mutation.set({ loading: false, error: null })
-    })
-    .catch(error => {
-      console.error(error)
-      this.props.mutation.set({ loading: false, error })
-    })
-  }
-
   render() {
-    const { loading, error, allImages } = this.props.data
-    if (loading) return <Loading />
-    else if (error) return <Error />
-
     return (
       <div>
-        { allImages.map(image => <img key={image.id} src={image.url} />) }
-        <div>
-          <Button
-            type="primary"
-            loading={this.props.mutation.loading}
-            onClick={this.createImageAndReFetch}
-          >
-            Load more...
-          </Button>
-        </div>
+        Gallery
       </div>
     )
   }
 }
 
-const withMutationState = mutationState()
-
-export default withMutationState(
-  compose(
-    graphql(mutation, { name: 'createImage' }),
-    graphql(query)
-)(Gallery))
+export default graphql(query)(Gallery)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo'
-import App from './components/App'
-import { logRequest } from './afterwares/logRequest'
 import 'antd/dist/antd.css'
+import App from './components/App'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj64vfbv3xxp80153vja60n7y',
-}).useAfter([ logRequest ])
+})
 
 const client = new ApolloClient({
   networkInterface: networkInterface,
-  dataIdFromObject: o => o.id
 })
 
 ReactDOM.render(
